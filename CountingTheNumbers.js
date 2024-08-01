dd5732c0ae8ed03781e1f7c5a51813dae3e79b50
Laimonas <ldirgelas530@gmail.com>
2024-08-01 12:41:30 +0300

    Initial commit

---
+++ CountingTheNumbers.js
@@ -0,0 +1,11 @@
+// Output
+// What is the input string? Homer
+// Homer has 5 characters
+
+let string = '';
+console.log(`What is the input string? ${string}`);
+
+//Ivedame reikšmė
+string='Homer';
+
+console.log(`Homer has ${string.length} characters.`);
